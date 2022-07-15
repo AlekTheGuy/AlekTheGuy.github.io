de1cc1d58d9be193e59497615633d300320fc6a4
Aleksander halvorsen Holthe <alek-hh@hotmail.com>
2022-07-15 15:21:08 +0200

    Complete experience component and fix site layout for flexbox

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,44 @@ function App() {
       <div className="contact-info">
         <img src={profilePhoto} className="avatar" />
         <div className="name">Aleksander Halvorsen Holthe</div>
+        <div className="links">
+          <a href="https://github.com/AlekTheGuy" target={"blank"}>
+            Github
+          </a>
+          <a
+            href="https://www.linkedin.com/in/aleksander-halvorsen-h-69a713128/"
+            target={"blank"}
+          >
+            Linkedin
+          </a>
+        </div>
       </div>
-      <Experience
-        img={
-          "https://innlandetfylke.no/kunde/grafikk/innlandet-fylkeskommune.svg"
-        }
-        title={"IT-support apprentice"}
-        info={"2017-2019 in Gjøvik, Norway"}
-      />
-      <Experience
-        img={
-          "https://innlandetfylke.no/kunde/grafikk/innlandet-fylkeskommune.svg"
-        }
-        title={"IT-support apprentice"}
-        info={"2017-2019 in Gjøvik, Norway"}
-      />
-      <Experience
-        img={
-          "https://innlandetfylke.no/kunde/grafikk/innlandet-fylkeskommune.svg"
-        }
-        title={"IT-support apprentice"}
-        info={"2017-2019 in Gjøvik, Norway"}
-      />
+      <h2>Arbeidserfaring</h2>
+      <div className="experience">
+        <Experience
+          img={
+            "https://innlandetfylke.no/kunde/grafikk/innlandet-fylkeskommune.svg"
+          }
+          title={"IT-support apprentice"}
+          info={"2017-2019 in Gjøvik, Norway"}
+        />
+        <Experience
+          img={
+            "https://innlandetfylke.no/kunde/grafikk/innlandet-fylkeskommune.svg"
+          }
+          title={"IT-support apprentice"}
+          info={"2017-2019 in Gjøvik, Norway"}
+        />
+        <Experience
+          img={
+            "https://innlandetfylke.no/kunde/grafikk/innlandet-fylkeskommune.svg"
+          }
+          title={"IT-support apprentice"}
+          info={"2017-2019 in Gjøvik, Norway"}
+        />
+      </div>
+      <h2>Utdannelse</h2>
+      <h2>Prøveresultater</h2>
     </div>
   );
 }
